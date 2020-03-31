012e8ca900289b7e812f8fe8d87753f83584fc0c
Kanika Mehra <s3688154@student.rmit.edu.au>
2020-03-31 23:55:00 +1100

    game stops when the max limit for points is reached by any player

--- TaskC/gameEngine.py
+++ TaskC/gameEngine.py
@@ -6,8 +6,9 @@ from time import sleep
 
 sense=SenseHat()
 
-MAX_POINTS=30
-firstPlayerIsActive=True
+MAX_POINTS=10
+bColour=(255,111,111)
+tColour=(111,255,111)
 
 class gameEngine:
     def __init__(self,die):
@@ -29,46 +30,42 @@ class gameEngine:
     def introToTheGame(self):
         welcome = "Welcome to the electronic die game. Press the joystick to go through the game's instructions."
         sense.show_message(welcome, scroll_speed=0)
-        bColour=(255,111,111)
         event = sense.stick.wait_for_event()
 
-        intro="Player 1 and Player 2 take turns in rolling a die. The first one to roll {0} points is the winner. Press joystick while switching between the players. Let's start the game".format(MAX_POINTS)
+        intro="Player 1 and Player 2 take turns in rolling a die. The first one to roll {0} points is the winner. Let's start the game.".format(MAX_POINTS)
         sense.show_message(intro, scroll_speed=0)
         sense.clear(bColour)
         sleep(1)
 
     def startGame(self):
         self.introToTheGame()
-        sense.show_message("{}'s turn".format(self.__playersList[0].getPlayerInfo().get("name")))
-        print("SAT INITIAL")
+        self.setInitialActivePlayerStatus()
         while self.__playersList[0].getPlayerInfo().get("points")<MAX_POINTS and self.__playersList[1].getPlayerInfo().get("points")<MAX_POINTS:
             self.__die.listenForShake() 
-            print("SHOOK")
-            self.setActivePlayerPointAndSwitchActivePlayer(self.__die.getFaceValue())
-            
-        
+            self.setActivePlayerPointAndSwitchActivePlayer(self.__die.getFaceValue())    
 
-    #def setInitialActivePlayerStatus(self):
-     #   self.__playersList[0].setActiveStatus(True)
-      #  sense.show_message("{}'s turn".format(self.__playersList[0].getPlayerInfo().get("name")))
-       # self.__playersList[1].setActiveStatus(False)
-        #print("1={}".format( self.__playersList[0].getActiveStatus()))
-        #print("2={}".format( self.__playersList[1].getActiveStatus()))
-
+    def setInitialActivePlayerStatus(self):
+       self.__playersList[0].setActiveStatus(True)
+       sense.show_message("{}'s turn".format(self.__playersList[0].getPlayerInfo().get("name")),text_colour=tColour)
+       self.__playersList[1].setActiveStatus(False)
 
     def setActivePlayerPointAndSwitchActivePlayer(self,points):
-        if self.firstPlayerIsActive:
+        activePlayerPoints=0
+        if self.__playersList[0].getActiveStatus():
             self.__playersList[0].addPoints(points)
-            sense.show_message("Points:{}".format(self.__playersList[0].getPlayerInfo().get("points")))
-            sense.show_message("{}'s turn".format(self.__playersList[1].getPlayerInfo().get("name")))
+            activePlayerPoints=self.__playersList[0].getPlayerInfo().get("points")
+            sense.show_message("Points:{}".format(activePlayerPoints))
+            if activePlayerPoints<MAX_POINTS:
+                sense.show_message("{}'s turn".format(self.__playersList[1].getPlayerInfo().get("name")),text_colour=tColour)
         else:
             self.__playersList[1].addPoints(points)
-            sense.show_message("Points:{}".format(self.__playersList[1].getPlayerInfo().get("points")))
-            sense.show_message("{}'s turn".format(self.__playersList[0].getPlayerInfo().get("name")))
+            activePlayerPoints=self.__playersList[1].getPlayerInfo().get("points")
+            sense.show_message("Points:{}".format(activePlayerPoints))
+            if activePlayerPoints<MAX_POINTS:
+                sense.show_message("{}'s turn".format(self.__playersList[0].getPlayerInfo().get("name")),text_colour=tColour)
 
-        self.firstPlayerIsActive=not self.firstPlayerIsActive
-        #self.__playersList[0].setActiveStatus(not self.__playersList[0].getActiveStatus())
-        #self.__playersList[1].setActiveStatus(not self.__playersList[1].getActiveStatus())
+        self.__playersList[0].setActiveStatus(not self.__playersList[0].getActiveStatus())
+        self.__playersList[1].setActiveStatus(not self.__playersList[1].getActiveStatus())
 
 
 
--- TaskC/player.py
+++ TaskC/player.py
@@ -5,12 +5,19 @@ class player:
     def __init__(self,name):
         self.__name=name
         self.__currPoints = 0
+        self.__activeStatus=False
 
     def getPlayerInfo(self):
         return {
             "name":self.__name,
             "points":self.__currPoints            
         }
+    
+    def getActiveStatus(self):
+        return self.__activeStatus
+
+    def setActiveStatus(self, activeStatus):
+        self.__activeStatus=activeStatus
     
     def addPoints(self, points):
         self.__currPoints += points
