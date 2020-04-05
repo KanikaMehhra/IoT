a3f0a989645b487c2a474ab48f5aae61828c4840
Kanika Mehra <s3688154@student.rmit.edu.au>
2020-04-05 23:03:53 +1000

    updated the anlytics file

--- TaskB/monitorAndDisplay.py
+++ TaskB/monitorAndDisplay.py
@@ -7,8 +7,10 @@ import os,sys,inspect
 current = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 parent = os.path.dirname(current)
 sys.path.insert(0, parent) 
-import constants
 
+RED=(255,0,0)
+GREEN=(0,255,0)
+BLUE=(0, 0, 255)
 sense = SenseHat()
 
 class MonitorAndDisplay: 
@@ -19,28 +21,36 @@ class MonitorAndDisplay:
         try:
             self.__data = DataInput(file).getData()
         except FileNotFoundError:
-            sense.show_message("Sorry, missing a config file", text_colour=constants.RED)
+            sense.show_message("Sorry, missing a config file", text_colour=RED)
             print("Configuration file not found, closing program...")
             sense.clear()
-            sys.exit()
+            self.quit()
 
         self.__cold_max = self.__data['cold_max'] 
         self.__comfortable_min = self.__data['comfortable_min'] 
         self.__comfortable_max = self.__data['comfortable_max'] 
         self.__hot_min = self.__data['hot_min'] 
 
+    def quit(self):
+        sense.clear()
+        sys.exit()
+    
     def runTemp(self):
         self.initData('config.json')
 
         while True:
             temp = self.__calib.get_calibrated_temp()
 
+            for event in sense.stick.get_events():
+                if event.action == 'pressed':
+                    self.quit()
+ 
             if temp <= self.__cold_max:
-                sense.show_message(str("%.1fC" % temp), text_colour=constants.RED)
+                sense.show_message(str("%.1fC" % temp), text_colour=RED)
             elif temp > self.__comfortable_min and temp < self.__comfortable_max:
-                sense.show_message(str("%.1fC" % temp), text_colour =constants.GREEN)
+                sense.show_message(str("%.1fC" % temp), text_colour =GREEN)
             elif temp >= self.__hot_min:
-                sense.show_message(str("%.1fC" % temp), text_colour = constants.RED) 
+                sense.show_message(str("%.1fC" % temp), text_colour = RED) 
     
             sleep(10)
             sense.clear()
