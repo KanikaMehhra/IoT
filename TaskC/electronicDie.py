caea723b2add3436a1b9482ba60c356ffd35fd1e
s3748189 <49212949+s3748189@users.noreply.github.com>
2020-04-05 23:15:58 +1000

    return to previous version

--- TaskC/electronicDie.py
+++ TaskC/electronicDie.py
@@ -19,29 +19,25 @@ class ElectronicDie:
     #function to listen for shaking of pi, once shaking detected, perform rolling die animation 
     #and set face value of the die.
     def listenForShake(self):
-        acceleration = sense.get_accelerometer_raw()
-        x = acceleration['x']
-        y = acceleration['y']
-        z = acceleration['z']
+        while True:		   
+            acceleration = sense.get_accelerometer_raw()
+            x = acceleration['x']
+            y = acceleration['y']		         
+            z = acceleration['z']		
 
-        x = round(x, 0)
-        y = round(y, 0)
-        z = round(z, 0)
-        
-        x = abs(x)
-        y = abs(y)
-        z = abs(z)
+ 
+            x = abs(x)
+            y = abs(y)
+            z = abs(z)		        
 
-        num = 0
-        if x > 1 or y > 1 or z > 1:
-            self.performAnimation()
-            num = randint(constants.MIN_DIE, constants.MAX_DIE)
-            self.setFaceValue(num)
-            sense.show_letter("{}".format(self.__faceValue),constants.T_COLOUR)
-            sleep(2)
-        
-        return num
-        
+ 
+            if x > 1 or y > 1 or z > 1:
+                self.performAnimation()	
+                self.setFaceValue(randint(1,6))
+                sense.show_letter("{}".format(self.__faceValue),constants.T_COLOUR)
+                sleep(2)
+                break
+                   
     #shows short animation to get the feeling of rolling a die 
     def performAnimation(self):
         interval=0.0
