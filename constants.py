dfb02223ee708e1c19bd1f5e5e1c143eb29bc924
s3748189 <49212949+s3748189@users.noreply.github.com>
2020-04-05 10:24:31 +1000

    Removed unnecessary values

--- constants.py
+++ constants.py
@@ -3,76 +3,8 @@ RED=(255,0,0)
 GREEN=(0,255,0)
 BLUE=(0, 0, 255)
 NO_COLOUR=(0,0,0)
-N = (0,0,0)
 WHITE=(255, 255, 255)
-W = (255, 255, 255)
 
 MIN_DIE = 1
 MAX_DIE = 6
-MAX_POINTS = 10
-
-ONE = [
-N, N, N, N, N, N, N, N,
-N, N, N, N, N, N, N, N,
-N, N, N, N, N, N, N, N,
-N, N, N, W, W, N, N, N,
-N, N, N, W, W, N, N, N,
-N, N, N, N, N, N, N, N,
-N, N, N, N, N, N, N, N,
-N, N, N, N, N, N, N, N
-]
-
-TWO = [
-N, N, N, N, N, N, N, N,
-N, N, N, N, N, W, W, N,
-N, N, N, N, N, W, W, N,
-N, N, N, N, N, N, N, N,
-N, N, N, N, N, N, N, N,
-N, W, W, N, N, N, N, N,
-N, W, W, N, N, N, N, N,
-N, N, N, N, N, N, N, N
-]
-
-THREE = [
-N, N, N, N, N, N, N, N,
-N, N, N, N, N, W, W, N,
-N, N, N, N, N, W, W, N,
-N, N, N, W, W, N, N, N,
-N, N, N, W, W, N, N, N,
-N, W, W, N, N, N, N, N,
-N, W, W, N, N, N, N, N,
-N, N, N, N, N, N, N, N
-]
-
-FOUR = [
-N, N, N, N, N, N, N, N,
-N, W, W, N, N, W, W, N,
-N, W, W, N, N, W, W, N,
-N, N, N, N, N, N, N, N,
-N, N, N, N, N, N, N, N,
-N, W, W, N, N, W, W, N,
-N, W, W, N, N, W, W, N,
-N, N, N, N, N, N, N, N
-]
-
-FIVE = [
-N, N, N, N, N, N, N, N,
-N, W, W, N, N, W, W, N,
-N, W, W, N, N, W, W, N,
-N, N, N, W, W, N, N, N,
-N, N, N, W, W, N, N, N,
-N, W, W, N, N, W, W, N,
-N, W, W, N, N, W, W, N,
-N, N, N, N, N, N, N, N
-]
-
-SIX = [
-N, N, N, N, N, N, N, N,
-N, N, N, N, N, N, N, N,
-N, W, W, N, N, W, W, N,
-N, N, N, N, N, N, N, N,
-N, W, W, N, N, W, W, N,
-N, N, N, N, N, N, N, N,
-N, W, W, N, N, W, W, N,
-N, N, N, N, N, N, N, N
-]
+MAX_POINTS = 30
